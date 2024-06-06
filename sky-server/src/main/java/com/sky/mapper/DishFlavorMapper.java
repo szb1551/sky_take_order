ff8fe84943deb86a124a89ed0ac27579ccffc9ca
szb1551 <2262400256@qq.com>
2024-06-06 21:46:06 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.sky.entity.DishFlavor;
+
+@Mapper
+public interface DishFlavorMapper {
+
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    public void insertBatch(List<DishFlavor> flavors);
+    
+}
