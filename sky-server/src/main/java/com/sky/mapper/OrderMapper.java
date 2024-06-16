6e2d0df9a64969ab18caa483747b43d496dddc16
szb1551 <2262400256@qq.com>
2024-06-16 21:54:49 +0800

    用户下单功能开发

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,13 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.sky.entity.Orders;
+
+@Mapper
+public interface OrderMapper {
+
+    void insert(Orders orders);
+
+    
+}
